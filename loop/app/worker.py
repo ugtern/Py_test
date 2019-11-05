8cc69ac391966bf843c942d30289ce4ad4824a73
sergej <ugtern@gmail.com>
2019-11-06 00:02:12 +0400

    настройки под локальное развертывание

--- loop/app/worker.py
+++ loop/app/worker.py
@@ -3,7 +3,6 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 from app.databases import MongoDB
-import logging
 
 
 class WorkerClass(MongoDB):
@@ -27,8 +26,6 @@ class WorkerClass(MongoDB):
 
             html_content = BeautifulSoup(res.content, 'html.parser')
             output = html_content.find_all('a', class_='storylink') # вытаскиваем все что соответствует классу, так как только нужные нам элементы на странице имеют такой класс
-
-            logging.info(output)
 
             for content in output: # заливаем данные в бд, из за требований к индексу пришлось обойтись без insert_many
                 db.py_test.insert({"_id": self.get_next_id('py_test_db', "py_test"),
